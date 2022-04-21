e3d7d590d95b999638c67d0a4204397cf53b0819
Lesley van Delft <lesleyvandelft92@gmail.com>
2022-04-21 15:50:49 +0200

    change app routes to components for animation

---
+++ frontend/src/components/AppRoutes.js
@@ -0,0 +1,29 @@
+import { Route, Routes, useLocation } from 'react-router-dom';
+// import Header from './components/Header/Header';
+import Homepage from '../pages/Homepage';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Create from '../pages/Create/Create';
+import Details from '../pages/Details/Details';
+import Edit from '../pages/Edit/Edit';
+import Test from '../pages/Test';
+import Cookies from 'js-cookie';
+import RoadmapPage from '../pages/Roadmap/Roadmap';
+
+const AppRoutes = () => {
+	const location = useLocation();
+	return (
+		<Routes location={location} key={location.pathname}>
+			<Route exact path="/" element={<Homepage />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/register" element={<Register />} />
+			<Route path="/details/:id" element={<Details />} />
+			<Route path="/edit/:id" element={<Edit />} />
+			<Route path="/create" element={<Create />} />
+			<Route path="/roadmap" element={<RoadmapPage />} />
+			<Route path="/test" element={<Test />} />
+		</Routes>
+	);
+};
+
+export default AppRoutes;
