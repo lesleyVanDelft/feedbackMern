ebaf7f4884f64790f95bdc8362c339c799a0caa8
Lesley van Delft <lesleyvandelft92@gmail.com>
2022-05-18 21:53:34 +0200

    heroku again

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ app.use('/api/feedbacks', feedbackRoutes);
 // Serve frontend
 // const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('../frontend/build'));
+	app.use(express.static(__dirname, '../frontend/build'));
 
 	app.get('/', (req, res) => {
 		res.sendFile(
