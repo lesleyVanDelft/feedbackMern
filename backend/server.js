63811da4cbdde23bfe1960e42ec4f0f9bff8721d
Lesley van Delft <lesleyvandelft92@gmail.com>
2022-05-21 22:44:35 +0200

    s3 bucket working

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const multer = require('multer');
 const colors = require('colors');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
@@ -11,10 +12,13 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 const feedbackRoutes = require('./routes/feedback');
 const { getFeedbacks } = require('./controllers/feedback');
+const { uploadFile, getFileStream } = require('./s3');
 
 connectDB();
 
 const app = express();
+
+const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -26,11 +30,31 @@ app.use(morgan('tiny'));
 app.use('/api/feedbacks', feedbackRoutes);
 app.use('/api/users', authRoutes);
 
+app.get('/images/:key', (req, res) => {
+	const key = req.params.key;
+	const readStream = getFileStream(key);
+
+	readStream.pipe(res);
+});
+app.post('/images', upload.single('image'), async (req, res) => {
+	const file = req.file;
+	console.log(file);
+
+	// apply filter
+	// resize
+
+	const result = await uploadFile(file);
+	await unlinkFile(file.path);
+	console.log(result);
+	const description = req.body.description;
+	res.send({ imagePath: `/images/${result.Key}` });
+});
+
 app.get('/login', (req, res) => {
 	res.status(301).redirect('https://feedback-lesley.herokuapp.com');
 });
-app.get('/roadmap', getFeedbacks);
 
+app.get('/roadmap', getFeedbacks);
 app.get('/roadmap', (req, res) => {
 	res.status(301).send('roadmap');
 });
--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -1,10 +1,48 @@
-import LoadingSpinner from '../components/LoadingSpinner/LoadingSpinner';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+
+const postImage = async ({ image, description }) => {
+	const formData = new FormData();
+	formData.append('image', image);
+	formData.append('description', description);
+
+	const result = await axios.post('/images', formData, {
+		headers: { 'Content-Type': 'multipart/form-data' },
+	});
+	return result.data;
+};
 
 const Test = () => {
+	const [file, setFile] = useState();
+	const [description, setDescription] = useState('');
+	const [images, setImages] = useState([]);
+	const feedbacks = useSelector(state => state.feedbacks);
+
+	const submit = async e => {
+		e.preventDefault();
+		const result = await postImage({ image: file, description });
+		setImages([result.image, ...images]);
+	};
+
+	const fileSelected = event => {
+		const file = event.target.files[0];
+		setFile(file);
+	};
+
 	return (
 		<>
-			<LoadingSpinner />
+			<form onSubmit={submit}>
+				<input onChange={fileSelected} type="file" accept="image/*"></input>
+				<input
+					value={description}
+					onChange={e => setDescription(e.target.value)}
+					type="text"></input>
+				<button type="submit">Submit</button>
+			</form>
+			{/* <img src="/images/488de294972ef2af03acab95322ad2f6" alt="" /> */}
 		</>
+		// <h1>test</h1>
 	);
 };
 
